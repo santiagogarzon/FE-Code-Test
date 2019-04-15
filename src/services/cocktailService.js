7c238a1f0d67109c3ffe634be97f06cc06934a2a
Santi Garzon <santiagogarzon04@gmail.com>
2019-04-14 22:36:21 -0300

    code-test implementation

---
+++ src/services/cocktailService.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import _ from 'lodash';
+
+export default class AppService{
+    static getCocktails() {
+        return axios.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass')
+        .then(res => {
+            return res.data.drinks;
+        })
+        .catch(err => {
+            return err;
+        })
+    }
+
+    static getCocktail(id) {
+        return axios.get('http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + id)
+        .then(res => {
+            return res.data.drinks[0];
+        })        
+        .catch(err => {
+            return err;
+        })
+    }
+
+    static contains(name, query) {
+        name = name.toLowerCase();
+        if(name.includes(query)) {
+            return true;
+        }
+
+        return false;
+    }
+}
